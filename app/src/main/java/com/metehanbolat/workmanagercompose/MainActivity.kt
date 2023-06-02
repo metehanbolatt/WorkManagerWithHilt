6c05199490c7d49734059367ee27a8a1641f13d8
Metehan <metehan.bolat.ie@gmail.com>
2023-06-02 16:50:24 +0300

    Initial commit

---
+++ app/src/main/java/com/metehanbolat/workmanagercompose/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.metehanbolat.workmanagercompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import com.metehanbolat.workmanagercompose.ui.theme.WorkManagerComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WorkManagerComposeTheme {
+
+            }
+        }
+    }
+}
